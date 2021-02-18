f0fc3245f307a4764d505003c5228ffda08ae127
iftearmi28 <77778913+iftearmi28@users.noreply.github.com>
2021-02-18 23:58:04 +0600

    birthday wish projects

---
+++ wish.js
@@ -0,0 +1,23 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  
+  slides[slideIndex-1].style.display = "block";  
+ 
+  setTimeout(showSlides, 2500); // Change image every 2 seconds
+}
+function wishTo() {
+    setTimeout(wishBd, 9000);
+}
+var wish=document.getElementById('afterwish');
+function wishBd() {
+    wish.style.display="none";
+}
